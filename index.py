e890102b4185f8621c01884eddf971e5cc36bf60
gwynethblanco <blancogwyneth@gmail.com>
2024-12-03 17:19:33 -0500

    fwefewr

--- index.py
+++ index.py
@@ -129,19 +129,17 @@ def crear_distribucion(data, title):
     return distri
 # ---------------------------------MODELOS-----------------------------------
 
-# Cargar el diccionario desde el archivo pickle
-with open(r'C:\Users\Usuario\Pictures\CARPETAPROYECTOFINAL\entrega2_proyectofinal\resultados_XGB.pkl', 'rb') as file:
+with open('C:/Users/Usuario/Pictures/CARPETAPROYECTOFINAL/entrega2_proyectofinal/resultados_XGB.pkl', 'rb') as file:
     resultados_xgboost = pickle.load(file)
 
-with open(r'C:\Users\Usuario\Pictures\CARPETAPROYECTOFINAL\entrega2_proyectofinal\resultados_svr_final.pkl', 'rb') as file:
+with open('C:/Users/Usuario/Pictures/CARPETAPROYECTOFINAL/entrega2_proyectofinal/resultados_svr_final.pkl', 'rb') as file:
     resultados_svr = pickle.load(file)
 
-with open(r'C:\Users\Usuario\Pictures\CARPETAPROYECTOFINAL\entrega2_proyectofinal\resultados_rf_final.pkl', 'rb') as file:
+with open('C:/Users/Usuario/Pictures/CARPETAPROYECTOFINAL/entrega2_proyectofinal/resultados_rf_final.pkl', 'rb') as file:
     resultados_rf = pickle.load(file)
 
-with open(r'C:\Users\Usuario\Pictures\CARPETAPROYECTOFINAL\entrega2_proyectofinal\resultados_hibrido_xgb_svr.pkl', 'rb') as file:
+with open('C:/Users/Usuario/Pictures/CARPETAPROYECTOFINAL/entrega2_proyectofinal/resultados_hibrido_xgb_svr.pkl', 'rb') as file:
     hybridmodel = pickle.load(file)
-    
 # Función para crear los gráficos con Plotly
 
 def graficasmodelos(observed, predicted, training_data, title):
